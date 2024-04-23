82be0a24e904b9dee0c0ca72d0953783ee40b860
Ana Garcia Montez <ana.garcia.montez@hva.nl>
2024-04-23 16:25:50 +0200

    het WERKT

---
+++ arduino/pointing.py
@@ -0,0 +1,22 @@
+from naoqi import ALProxy
+tts = ALProxy("ALTextToSpeech", "145.3.253.53", 9559)
+tts.say("Hello, world!")
+
+# // import com.aldebaran.qi.helper.proxies.ALMotion;
+
+# // import java.util.Scanner;
+
+# // public class Main {
+
+# //     public static void main(String[] args) throws Exception {
+# //         NAO nao = new NAO();
+# //         Scanner scanner = new Scanner(System.in);
+# //         nao.verbinden("localhost",50109);   // verbinden met de virtuele robot in choreogrape
+# //         System.out.println(nao.watKanIk());
+# //         System.out.println();
+# //         System.out.print("Wat wil je dat ik doe: ");
+# //         String movement = scanner.nextLine();
+# //         nao.allesWatJeWil(movement);
+# //     }
+# // }
+
