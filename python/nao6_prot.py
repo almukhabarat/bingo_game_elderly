04ffbc7fc29e2df5b6ecf48c69926722c0862350
kirchob <kirchob@hva.nl>
2024-04-24 20:50:51 +0200

    brian scorion exp sprint1

--- python/nao6_prot.py
+++ python/nao6_prot.py
@@ -1,7 +1,7 @@
 from naoqi import ALProxy
 
-tts = ALProxy("ALTextToSpeech", "169.254.56.184", 9559)
-motion = ALProxy( "ALMotion", "169.254.56.184", 9559)
+tts = ALProxy("ALTextToSpeech", "169.254.30.75", 9559)
+motion = ALProxy( "ALMotion", "169.254.30.75", 9559)
 
 threadMove = motion.post.moveTo( 1.0, 0.0, 0.0 )
 tts.say("hi ik werk")
