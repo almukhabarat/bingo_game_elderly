e019f7f2854f566c9788a4c2ba931e85834d4b88
Ana Garcia Montez <ana.garcia.montez@hva.nl>
2024-04-24 14:36:33 +0200

    python

--- python/nao6_prot.py
+++ python/nao6_prot.py
@@ -1,3 +1,11 @@
 from naoqi import ALProxy
+
 tts = ALProxy("ALTextToSpeech", "169.254.56.184", 9559)
+motion = ALProxy( "ALMotion", "169.254.56.184", 9559)
+
+threadMove = motion.post.moveTo( 1.0, 0.0, 0.0 )
+tts.say("hi ik werk")
+
+motion.wait(threadMove, 0)
+
 tts.say("hi ik werk")
