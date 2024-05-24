4b1bb62c263057d70633f2a753cf09fe6a3c8695
garciaa8 <ana.garcia.montez@hva.nl>
2024-05-24 14:19:37 +0200

    flask

--- arduino/knop.cpp
+++ arduino/knop.cpp
@@ -1,3 +1,10 @@
+#include <WiFi.h>
+#include "kl3z5mgm.h"
+#include "FlaskHttp.h"
+
+#define SERVER "http://145.92.8.134"
+#define END_POINT "/post_command"
+
 class Buzzer {
   private:
     int pin;
@@ -11,7 +18,7 @@ class Buzzer {
     }
 
     void playTone(int frequency, int duration) {
-      ledcWriteTone(channel, frequency);  // Play tone
+      ledcWriteTone(channel, frequency); // Play tone
       delay(duration);
       ledcWriteTone(channel, 0); // Stop tone
     }
@@ -35,21 +42,46 @@ class Button {
     }
 };
 
-Buzzer buzzer(15, 0); // Instantiate Buzzer object with pin 15 and channel 0
-Button button(14);    // Instantiate Button object with pin 14
+Buzzer buzzer1(15, 0); // Instantiate Buzzer object with pin 15 and channel 0
+Buzzer buzzer2(6, 1);  // Instantiate second Buzzer object with pin 6 and channel 1
+Button button(14);     // Instantiate Button object with pin 14
+
+FlaskHttp flaskHttp(SERVER, END_POINT);
 
 void setup() {
+  // Seriële monitor
+  Serial.begin(115200);
+
+  // Wifi configuratie
+  WiFi.begin(H_N, H_A);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("Wi-Fi connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  flaskHttp.begin();
 }
 
 void loop() {
   if (button.isPressed()) {
-    buzzer.playTone(2000, 100);
-    buzzer.playTone(1000, 100);
-    buzzer.playTone(1500, 100);
-    buzzer.playTone(3000, 100);
-    buzzer.playTone(500, 100);
+    buzzer1.playTone(2000, 100);
+    buzzer2.playTone(2000, 100);
+    buzzer1.playTone(1000, 100);
+    buzzer2.playTone(1000, 100);
+    buzzer1.playTone(1500, 100);
+    buzzer2.playTone(1500, 100);
+    buzzer1.playTone(3000, 100);
+    buzzer2.playTone(3000, 100);
+    buzzer1.playTone(500, 100);
+    buzzer2.playTone(500, 100);
+
+    flaskHttp.postCommand("button_pressed");
   } else {
-    buzzer.stopTone();
+    buzzer1.stopTone();
+    buzzer2.stopTone();
   }
 
   delay(50);
